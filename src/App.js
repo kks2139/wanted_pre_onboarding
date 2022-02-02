27f654f4ebf3408cd9f807b997df5154af6bbd66
kwangsun <eyeyel@naver.com>
2022-02-03 00:08:11 +0900

    세팅

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Toggle from "./component/Toggle";
+
+function App() {
+  return (
+    <div className="App">
+      <Toggle/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Toggle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {css} from '@emotion/react';
+
+function Toggle(){
+
+
+    const style = css`
+
+    `;
+
+    return(
+        <div css={style}>
+            toggle
+        </div>
+    );
+}
+
+export default Toggle;
